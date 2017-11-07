b8876d18309c9b02a984c877ec724987233c5719
peanutbutter27 <31664764+peanutbutter27@users.noreply.github.com>
2017-11-07 15:03:44 -0500

    Add files via upload

---
+++ pwm.ino
@@ -0,0 +1,26 @@
+const int MAX_8_INT = 255; 
+int brightness;
+int fade_amount;
+
+
+
+void setup() {
+brightness = 0;  // initialize brightness to 0 
+fade_amount = 1; // initialize fade_amount to 1
+analogReadResolution(12);
+pinMode(10, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int brightness = analogRead(A0);
+  analogWrite(LED_BUILTIN, brightness >> 4); 
+  digitalWrite(10,brightness);
+/*  brightness = brightness + fade_amount;
+
+if (brightness ==0 or brightness == MAX_8_INT)
+  {
+  fade_amount = -fade_amount;
+  }
+  delay(100); // how much time in milliseconds*/
+}
